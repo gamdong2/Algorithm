5f2e8b568d4ec384474611e024502c4aa9531bc7
신유라 <boar7027@gmail.com>
2024-12-28 11:37:01 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -0,0 +1,10 @@
+def solution(n):
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+    def lcm(a, b):
+        return a * b // gcd(a, b)
+
+    return lcm(6, n) // 6
